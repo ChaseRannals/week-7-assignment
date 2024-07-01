d09d5a14acf29a56cee7d1089b8cc9d1cec3ca20
ChaseRannals <chaserannals@gmail.com>
2024-07-01 08:53:56 -0500

    added comments

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
 //1. Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
-//Programmatically subtract the value of the first element in the array from the value in the last element of the array.
+//Programmatically subtract the value of the first element in the array from the value in the 
+//last element of the array.
 //Do not use numbers to reference the last element, find it programmatically.
 //ages[7] - ages[0] is not allowed!
-//Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
+//Add a new age to your array and repeat the step above to ensure it is dynamic. (works for 
+//arrays of different lengths).
 //Use a loop to iterate through the array and calculate the average age.
 
 let ages = [3, 9, 23, 64, 2, 8, 28, 93];
@@ -25,10 +27,12 @@ for (let i = 0; i < ages.length; i++) {
 let average = sum / ages.length;
 console.log("Average age:", average);
 
-//2. Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck',
+//2. Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim',
+// 'Sally', 'Buck',
 //   'Bob'.
 //Use a loop to iterate through the array and calculate the average number of letters per name.
-//Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.
+//Use a loop to iterate through the array again and concatenate all the names together, separated
+// by spaces.
 
 let names = ['sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'];
 
@@ -51,14 +55,17 @@ for (let i = 0; i < names.length; i++) {
 console.log("Concatenated names:", concatenatedNames);
 
 //3. How do you access the last element of any array?
-//Answer: By using the - 1 index.  By using the array[array.length - 1], you can access the last element in the array.
+//Answer: By using the - 1 index.  By using the array[array.length - 1], you can access the last 
+//element in the array.
 
 
 
 //4. How do you access the first element of any array?
-//Answer: By using the 0 index.  By using the array[0], you can access the first element in the array.
+//Answer: By using the 0 index.  By using the array[0], you can access the first element in the 
+//array.
 
-//5. Create a new array called nameLengths. Write a loop to iterate over the previously created names array 
+//5. Create a new array called nameLengths. Write a loop to iterate over the previously created 
+//names array 
 //   and add the length of each name to the nameLengths array.
 //For example:
 
@@ -73,8 +80,8 @@ console.log("Concatenated names:", concatenatedNames);
 //}
 //console.log(nameLengths); //produces the amount of letters in the names
 
-//6. Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the 
-//   array.
+//6. Write a loop to iterate over the nameLengths array and calculate the sum of all the 
+//elements in the array.
 
 //let sum = 0;
 
@@ -83,9 +90,9 @@ console.log("Concatenated names:", concatenatedNames);
 //}
 //console.log("Sum")
 
-//7. Write a function that takes two parameters, word and n, as arguments and returns the word concatenated 
-//to itself n number of times. (i.e. if I pass in 'Hello' and 3, I would expect the function to 
-//return 'HelloHelloHello').
+//7. Write a function that takes two parameters, word and n, as arguments and returns the word 
+// concatenated to itself n number of times. (i.e. if I pass in 'Hello' and 3, I would expect 
+//the function to return 'HelloHelloHello').
 
 function concatenateWord(word, n) {
     let result = '';
@@ -105,8 +112,8 @@ function fullName(firstName, lastName) {
 }
 console.log(fullName('Chase', 'Rannals'));
 
-//9. Write a function that takes an array of numbers and returns true if the sum of all the numbers in the 
-//array is greater than 100.
+//9. Write a function that takes an array of numbers and returns true if the sum of all the 
+//numbers in the array is greater than 100.
 
 function sumOfAll(numbers) {
     let sum = 0;
@@ -119,8 +126,8 @@ function sumOfAll(numbers) {
 console.log(sumOfAll([12, 40, 90]));
 console.log(sumOfAll([5, 10, 30]));
 
-//10. Write a function that takes an array of numbers and returns the average of all the elements in the 
-//array.
+//10. Write a function that takes an array of numbers and returns the average of all the 
+//elements in the array.
 
 function sumOfNumbers(numbers) {
     let sum = 0;
@@ -133,8 +140,8 @@ function sumOfNumbers(numbers) {
 console.log(sumOfNumbers([10, 20, 40, 50]));
 console.log(sumOfNumbers([50, 60, 70, 80, 90]));
 
-//11. Write a function that takes two arrays of numbers and returns true if the average of the elements in 
-//the first array is greater than the average of the elements in the second array.
+//11. Write a function that takes two arrays of numbers and returns true if the average of the 
+//elements in the first array is greater than the average of the elements in the second array.
 
 function compareArrays(arr1, arr2) {
     let sum1 = 0;
@@ -153,8 +160,8 @@ function compareArrays(arr1, arr2) {
 console.log(compareArrays([20, 15, 30, 25], [40, 70, 25, 20]));
 console.log(compareArrays([40, 50, 20, 15], [30, 20, 15, 40]));
 
-//12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, 
-//and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
+//12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number 
+//moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 
 function willBuyDrink(isHotOutside, moneyInPocket) {
     return isHotOutside && moneyInPocket > 10.50;
@@ -165,8 +172,8 @@ console.log(willBuyDrink(false, 20));
 console.log(willBuyDrink(false, 2));
 
 
-//13. Create a function of your own that solves a problem. In comments, write what the function does and 
-//why you created it.
+//13. Create a function of your own that solves a problem. In comments, write what the function 
+//does and why you created it.
 
 function getBirthYear(age) {
     const currentYear = new Date().getFullYear();
@@ -174,3 +181,5 @@ function getBirthYear(age) {
 }
 const myAge = 38;
 console.log("I was born in " + getBirthYear(myAge));
+
+//This tells you what year you are born in.
